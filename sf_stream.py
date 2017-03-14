5269a7dc0dafdfb99ef8f4aeee9d7d4aa33e79cc
Julia Dolgova <zorra@mail.ru>
2017-03-14 14:37:09 +0900

    Добавлен sf_stream для импорта с простой фильтрацией из почты на случай сбоя

---
+++ sf_stream.py
@@ -0,0 +1,16 @@
+from stream import *
+
+
+def manage_file_my(fn):
+    try:
+        manage_file(fn)
+    except:
+        print 'error while uploading {]'.format(fn)
+
+
+def filter_regs_from_stream_my(message):
+    return 'Daily_Registry' in message["subject"]
+    # return True
+
+
+utils.check_files_in_mail(settings.MAIL_POP3, filter_regs_from_stream_my, manage_file_my)
